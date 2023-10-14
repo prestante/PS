050a3d5096d096b9648885d9cf6cc6ace590b76f
prestante <prestante@yandex.ru>
2023-10-14 03:17:31 -0400

    replaces olf test code with the code for Removing Run Warnings

--- test.ps1
+++ test.ps1
@@ -1,5 +1,16 @@
-Get-Process -Name pwsh
-Get-Process chrome
-Get-Service diagsvc
-# CHANGES from mne
-# and changes from wtl
+$address = 'wtlnas1.wtldev.net'
+$domain = $address -replace '.*\.(\w+\.\w+$)','$1'
+$place = $address -replace '(.*)\.\w+\.\w+$','$1'
+$registryPaths = @()
+$registryPaths += "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\EscDomains"
+$registryPaths += "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\Domains"
+
+$name2 = "file"
+$value2 = 1
+
+foreach ($registryPath in $registryPaths) { 
+    if (!(Test-Path $registryPath)) {New-Item -Path $registryPath -Force | Out-Null}
+    if (!(Test-Path "$registryPath\$domain")) {New-Item -Path "$registryPath\$domain" -Force | Out-Null}
+    if (!(Test-Path "$registryPath\$domain\$place")) {New-Item -Path "$registryPath\$domain\$place" -Force | Out-Null}
+    if ((Get-Item "$registryPath\$domain\$place").property -notcontains $name2) {New-ItemProperty -Path "$registryPath\$domain\$place" -Name $name2 -Value $value2 -PropertyType DWORD -Force | Out-Null}
+}
